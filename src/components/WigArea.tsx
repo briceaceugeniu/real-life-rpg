dc9a3e36c0c39bf219391d8f3fffdffc8e199591
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2022-02-12 18:09:31 +0100

    birthday card

--- src/components/WigArea.tsx
+++ src/components/WigArea.tsx
@@ -1,11 +1,49 @@
-import React from "react";
-import CoronavirusIcon from "@mui/icons-material/Coronavirus";
-import { Button } from "@mui/material";
+import React, { useState } from "react";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
 const WigArea = () => {
+  const [msgVisible, setMsgVisible] = useState(false);
+
   return (
-    <div>
-      <CoronavirusIcon color="primary" sx={{ fontSize: 50 }} />
-      <Button variant="contained">Hello World</Button>
+    <div style={{ marginTop: "50px" }}>
+      <Button variant="contained" onClick={() => setMsgVisible(!msgVisible)}>
+        Click here!
+      </Button>
+      {msgVisible && (
+        <div className="cardCenter">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="250"
+                image="https://media1.giphy.com/media/g5R9dok94mrIvplmZd/giphy.gif?cid=790b7611fb061b8b1c04bc26e91c23b15773d23be97ab9f5&rid=giphy.gif&ct=g"
+                alt="g"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  La multi ani!!!
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <CelebrationIcon sx={{ color: "green" }} />
+                  <CelebrationIcon sx={{ color: "red" }} />
+                  <CelebrationIcon sx={{ color: "blue" }} />
+                  <CelebrationIcon sx={{ color: "orange" }} />
+                  <CelebrationIcon sx={{ color: "yellow" }} />
+                  <h1>35</h1>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+      )}
     </div>
   );
 };
