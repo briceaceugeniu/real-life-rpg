7073d3680b6d47ead147ce87e3fc43fb93f5d5dd
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2022-08-19 16:54:24 +0200

    change version label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <Container>
       <div className={`header-txt d-flex space-btwn`}>
         <div>Real Life RPG Game</div>
-        <small>v 0.001 PreAlfa</small>
+        <small>v 0.01 Alfa</small>
       </div>
     </Container>
   );
