7c789c2d4f38f2d00ea5283591e834260bbe42cc
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2022-08-19 18:54:40 +0200

    add icon link to github proj repo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Container } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Header = () => {
+  const openInNewTab = (url: string | URL | undefined) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <Container>
       <div className={`header-txt d-flex space-btwn`}>
         <div>Real Life RPG Game</div>
-        <small>v 0.01 Alfa</small>
+        <div className={`d-flex center-div`}>
+          <small>v 0.01 Alfa</small>
+          <GitHubIcon
+            sx={{ marginLeft: "10px" }}
+            color={`action`}
+            className={`github-icon`}
+            onClick={() =>
+              openInNewTab("https://github.com/briceaceugeniu/real-life-rpg-UI")
+            }
+          />
+        </div>
       </div>
     </Container>
   );
